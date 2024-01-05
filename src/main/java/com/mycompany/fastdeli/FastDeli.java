40b2f3862d3ab31586a4921ad8da1b49f0ac2941
tizibienvenus <tizibienvenus@gmail.com>
2024-01-05 23:42:34 +0100

    Firts Commit

---
+++ src/main/java/com/mycompany/fastdeli/FastDeli.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fastdeli;
+
+/**
+ *
+ * @author tizi
+ */
+public class FastDeli {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
